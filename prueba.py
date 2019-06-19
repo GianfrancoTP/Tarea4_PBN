64a7c9b78dd0d3da57d20bbea197a93c5fe3400c
GainfrancoTP <gstraverso@miuandes.cl>
2019-06-19 16:54:57 -0400

    cleaning code

--- prueba.py
+++ prueba.py
@@ -1,5 +1,5 @@
 #lo que Ud. tiene que programar
-from gravitacion import *
+from gravitacion import sim
 
 #para dibujar
 import time
@@ -12,12 +12,12 @@ ax = fig.add_subplot(111)
 
 #crea la simulacion
 r_disco=3e12
-s=sim(1.988e30, 1.898e27,  7.7857e11, 500, r_disco)
+s=sim(1.988e30, 1.898e27,  7.7857e11, 5000, r_disco)
 fig.show()
 
 i = 0
 #simula 4600 dias
-while i < 300:
+while i < 100:
 
 	r=s.run(20)
 
@@ -25,13 +25,13 @@ while i < 300:
 	xx, yy = zip(*[[c.x, c.y] for c in r])
 
 	#dibuja
-	pt.plot(xx[0],yy[0],'r.')
-	pt.plot(xx[2:],yy[2:],'b.')
-	pt.plot(xx[1],yy[1],'g.')
+	pt.plot(xx[0],yy[0],'r.',markersize=12)
+	pt.plot(xx[2:],yy[2:],'b.',markersize=1)
+	pt.plot(xx[1],yy[1],'g.',markersize=5)
 	pt.xlim(-r_disco,r_disco)
 	pt.ylim(-r_disco,r_disco)
 	fig.canvas.draw()
-	time.sleep(0.005)
+	time.sleep(0.01)
 	fig.clf()
 	i += 1
 
